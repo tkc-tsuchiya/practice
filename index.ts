f947d99abcc9efba599f7ddd1aa102fea258afd6
tkc <tkc.tsuchiya@gmail.com>
2021-12-14 13:24:52 +0900

    add code

---
+++ index.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env ts-node
+
+import { fetch } from "fetch-h2"
+
+// Determine whether the sentiment of text is positive
+// USe a web service
+async function isPositive(text: string): Promise<boolean> {
+    
+}
---
+++ sample.php
@@ -0,0 +1,8 @@
+<?php
+
+class Human {
+    public function __construct(Type $var = null) {
+        $this->var = $var;
+    }
+    public function 
+}
